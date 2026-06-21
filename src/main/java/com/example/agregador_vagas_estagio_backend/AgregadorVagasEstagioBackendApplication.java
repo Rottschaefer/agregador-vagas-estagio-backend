d7f15d337a2387b983c15ab4509f08ea04517c2d
Rottschaefer <rottschaefer54@gmail.com>
2026-06-21 19:00:48 -0300

    Meu primeiro commit

---
+++ src/main/java/com/example/agregador_vagas_estagio_backend/AgregadorVagasEstagioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.agregador_vagas_estagio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorVagasEstagioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorVagasEstagioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agregador_vagas_estagio_backend/AgregadorVagasEstagioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agregador_vagas_estagio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorVagasEstagioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
