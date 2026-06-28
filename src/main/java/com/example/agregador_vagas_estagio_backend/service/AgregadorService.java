b333330d66699e43ce8dd0d41e1e110f4ee5bb36
Camille Sá <camille.perazzini@gmail.com>
2026-06-28 20:28:05 -0300

    Implementando a busca geral de vagas

--- src/main/java/com/example/agregador_vagas_estagio_backend/service/AgregadorService.java
+++ src/main/java/com/example/agregador_vagas_estagio_backend/service/AgregadorService.java
@@ -6,11 +6,21 @@ import com.example.agregador_vagas_estagio_backend.interfaces.VagaScraper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class AgregadorService {
+
+    String[] fontes = {
+        "Gupy",
+        "Glassdoor",
+        "InfoJobs",
+        "EstagiarBR",
+        "AcademiaUniversitario"
+    };
 
     // Injeta todos os services que implementam VagaScraper aqui automaticamente
     @Autowired
@@ -19,6 +29,10 @@ public class AgregadorService {
     //Busca apenas na fonte especificada na URL
     public List<VagaDTO> buscaPorFonte(String fonte, String termo, String local) {
         // Monta o nome do Bean esperado (Ex: se vier "gupy", vira "gupyService")
+        System.out.print(fonte);
+
+        // if (fonte == "") return buscaVagas(termo, local);
+        
         String beanName = fonte.toLowerCase() + "Service";
         
         VagaScraper scraper = scrapers.get(beanName);
@@ -30,5 +44,27 @@ public class AgregadorService {
         return scraper.retornaVagas(termo, local);
     }
 
+    public List<VagaDTO> buscaVagas(String termo, String local) {
+        // Monta o nome do Bean esperado (Ex: se vier "gupy", vira "gupyService")
+        
+        List<VagaDTO> vagas = new ArrayList<>();
+        
+        for (String fonte : fontes) {
+            String beanName = fonte.toLowerCase() + "Service";
+
+            VagaScraper scraper = scrapers.get(beanName);
+
+            if (scraper == null) {
+                throw new IllegalArgumentException("Fonte de vagas inválida: " + fonte);
+            }
+
+            vagas.addAll(scraper.retornaVagas(termo, local));
+
+        }
+        
+        Collections.shuffle(vagas);
+        
+        return vagas;
+    }
         
 }
