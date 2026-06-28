78a479e385f32b11919580b465591d9d233d60bd
Rottschaefer <rottschaefer54@gmail.com>
2026-06-28 10:31:50 -0300

    Cross origin adicionado no controller

--- src/main/java/com/example/agregador_vagas_estagio_backend/controller/BuscaVagas.java
+++ src/main/java/com/example/agregador_vagas_estagio_backend/controller/BuscaVagas.java
@@ -3,6 +3,7 @@ package com.example.agregador_vagas_estagio_backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.agregador_vagas_estagio_backend.dto.VagaDTO;
 import com.example.agregador_vagas_estagio_backend.service.AgregadorService;
 
+@CrossOrigin("http://localhost:5173")
 @RestController
 @RequestMapping("buscar-vagas")
 public class BuscaVagas {
